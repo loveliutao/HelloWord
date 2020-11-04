2d8e141d31fe797526bf76f7ac798d764f37b59e
liutao <315333569@qq.com>
2020-11-04 23:19:14 +0800

    test ci

---
+++ src/main/java/com/hik/MainContorl.java
@@ -0,0 +1,15 @@
+package com.hik;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ajax")
+public class MainContorl {
+
+	@RequestMapping("/say")
+	public String sayHello()
+	{
+		return "hello,Mr liutao!";
+	}
+}
